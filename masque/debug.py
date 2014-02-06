4b647d2d49eba71bd3fb0f140a84c8b7efa1eb58
Andrei <andrei.zhabinski@gmail.com>
2014-02-06 08:38:43 +0300

    visaul debugging

--- masque/debug.py
+++ masque/debug.py
@@ -6,9 +6,23 @@ Partially based on:
 
 import numpy as np
 import matplotlib.pylab as plt
-from PIL import Image
+# from PIL import Image
+from masque.utils import implot
+
 
 def sigmoid(xx):
     return .5 * (1 + np.tanh(xx / 2.))
 
-    
+
+
+def plot_activ_prob(rbm, X):
+    """Plots activation probability of RBM's hidden neurons for input X"""
+    Xt = rbm.transform(X)
+    implot(X)
+
+
+
+
+# from masque import datasets
+# X, y = datasets.cohn_kanade_shapes()
+# from sklearn.neural_network import BernoulliRBM
--- masque/playground/patch_rbm.py
+++ masque/playground/patch_rbm.py
@@ -6,17 +6,20 @@ from sklearn.neural_network import BernoulliRBM
 from sklearn.svm import SVC
 from masque.playground.runners import pretrain_conv
 from masque.transform import PatchTransformer
+from masque import datasets
 
+
+_IM_SHAPE = (64, 64)
 
 patch_rbm_svc = {
     'pretrain_model' : Pipeline([
-        ('patch_trans', PatchTransformer((128, 128), (12, 12), n_patches=10)),
+        ('patch_trans', PatchTransformer(_IM_SHAPE, (12, 12), n_patches=10)),
         ('rbm', BernoulliRBM(n_components=72, verbose=True)),
     ]),
     'model' : SVC(kernel='linear', verbose=True),
-    'pretrain_data' : lambda: datasets.cohn_kanade(im_shape=(128, 128)),
-    'data' : lambda: datasets.cohn_kanade(im_shape=(128, 128),
+    'pretrain_data' : lambda: datasets.cohn_kanade(im_shape=_IM_SHAPE),
+    'data' : lambda: datasets.cohn_kanade(im_shape=_IM_SHAPE,
                                      labeled_only=True),
-    'x_shape' : (128, 128),
+    'x_shape' : _IM_SHAPE,
     'filter_shape' : (12, 12),
 }
